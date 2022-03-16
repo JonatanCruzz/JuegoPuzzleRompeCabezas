0b46659cadf6f49370f301d653e2ed0f78a4fa25
Jonatan <jonatanalexanderdiaz@gmail.com>
2022-03-16 16:26:43 -0400

    se subio el proyecto

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void Jugar(int Nivel)
+    {
+        PlayerPrefs.SetInt("Nivel", Nivel);
+        SceneManager.LoadScene("Juego");
+    }
+   
+}
---
+++ Assets/Scripts/pieza.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class pieza : MonoBehaviour
+{
+    private Vector3 PosicionCorrecta;
+    public bool piezaEncajada;
+    public bool piezaSeleccionada;
+
+    void Start()
+    {
+        PosicionCorrecta = transform.position;
+        transform.position = new Vector3(Random.Range(5f, 11f), Random.Range(2.5f, -7));
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, PosicionCorrecta) < 0.5f)
+        {
+            if (!piezaSeleccionada)
+            {
+                if (piezaEncajada == false)
+                {
+                    transform.position = PosicionCorrecta;
+                    piezaEncajada = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                    Camera.main.GetComponent<juego>().PiezasEncajadas++;
+                }
+            }
+        }
+    }
+}
